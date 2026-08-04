80ce23a2d51fc3f98881fb39e1139c48a57a7b82
Mu1hammed <m.elnshar@pksaudi.com>
2026-08-04 20:31:06 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,16 +7,7 @@
     @include('partials.head')
 
 <body>
-<!-- Google Tag Manager (noscript) -->
 
-<!-- End Google Tag Manager (noscript) -->
-@dd(config('services.google.tag_manager_id'))
-@if(config('services.google.tag_manager_id'))
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNM8X9B8"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-@endif
 @include('partials.navbar')
 
 @yield('content')
