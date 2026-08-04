f9d8671f50f0f3e75dd1c12286be37e3f0cc1d8f
Mu1hammed <m.elnshar@pksaudi.com>
2026-08-04 18:54:40 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 <!-- Google Tag Manager (noscript) -->
 
 <!-- End Google Tag Manager (noscript) -->
+@dd(config('services.google.tag_manager_id'))
 @if(config('services.google.tag_manager_id'))
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNM8X9B8"
